e8792024514fb795038ff79376b9bb57db8c92d4
Admin <Admin@172.25.7.29>
2026-04-22 08:21:33 +0700

    hihi

--- src/applicationprocess/LinearEquationSolver.java
+++ src/applicationprocess/LinearEquationSolver.java
@@ -1,18 +1,11 @@
 package applicationprocess;
 import java.util.*;
 public class LinearEquationSolver {
- public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.print("Nhap a: ");
-        double a = sc.nextDouble();
-        
-        System.out.print("Nhap b: ");
-        double b = sc.nextDouble();
-        
+ public class PhuongTrinh {
+    public PhuongTrinh(double a, double b) {
         if (a != 0) {
             double x = -b / a;
-            System.out.println("Phuong trinh " + a + "x + " + b + " = 0 co nghiem la: x = " + x);
+            System.out.println("Phuong trinh " + a + "x + " + b + " = 0 co nghiem: x = " + x);
         } else {
             if (b == 0) {
                 System.out.println("Phuong trinh 0x + 0 = 0 vo so nghiem");
@@ -20,8 +13,7 @@ public class LinearEquationSolver {
                 System.out.println("Phuong trinh 0x + " + b + " = 0 vo nghiem");
             }
         }
-        
-        sc.close();
-    }  
+    }
+ }
 }
-
+    
